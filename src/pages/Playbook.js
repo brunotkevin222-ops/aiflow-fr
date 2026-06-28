b676fd0124807d692560827bdcb86f70f026543d
brunotkevin222-ops <brunotkevin222@gmail.com>
2026-06-28 13:37:52 +0200

    Refactor imports in Playbook.js

--- src/pages/Playbook.js
+++ src/pages/Playbook.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Routes, Route, Link, useLocation } from 'react-router-dom';
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import './Playbook.css';
 
 const CHAPTERS = [
